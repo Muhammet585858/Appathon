d2e6a15a7618fbe4f9e8cf6cf131692b8b427f37
Muhammet58 <muhammetozer58@gmail.com>
2016-11-22 18:55:16 +0100

    new bg and mic button

--- app/src/main/java/com/example/muhammet/appathon/MainActivity.java
+++ app/src/main/java/com/example/muhammet/appathon/MainActivity.java
@@ -7,6 +7,7 @@ package com.example.muhammet.appathon;
 
             @Override
             protected void onCreate(Bundle savedInstanceState) {
+                setTheme(R.style.AppTheme);
                 super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
     }
