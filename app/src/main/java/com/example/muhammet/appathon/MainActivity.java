906ddc1bdff430600850d18decce2f38a2470760
Muhammet58 <muhammetozer58@gmail.com>
2016-11-23 20:50:27 +0100

    new bg and mic button

--- app/src/main/java/com/example/muhammet/appathon/MainActivity.java
+++ app/src/main/java/com/example/muhammet/appathon/MainActivity.java
@@ -1,14 +1,24 @@
 package com.example.muhammet.appathon;
 
+        import android.content.Intent;
         import android.support.v7.app.AppCompatActivity;
         import android.os.Bundle;
+        import android.view.View;
 
-        public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity {
 
             @Override
             protected void onCreate(Bundle savedInstanceState) {
                 setTheme(R.style.AppTheme);
                 super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
-    }
+                setContentView(R.layout.activity_main);
+            }
+
+            public void onButtonClick(View v){
+                if(v.getId() == R.id.button2){
+                    Intent i = new Intent(MainActivity.this, Microphone.class);
+                    startActivity(i);
+
+                }
+            }
 }
--- app/src/main/java/com/example/muhammet/appathon/Microphone.java
+++ app/src/main/java/com/example/muhammet/appathon/Microphone.java
@@ -1,8 +1,34 @@
 package com.example.muhammet.appathon;
+import android.app.Activity;
+import android.os.Bundle;
+
+import com.jjoe64.graphview.GraphView;
+import com.jjoe64.graphview.series.DataPoint;
+import com.jjoe64.graphview.series.LineGraphSeries;
 
 /**
  * Created by Muhammet on 22-11-2016.
  */
 
-public class Microphone {
+public class Microphone extends MainActivity {
+
+    LineGraphSeries<DataPoint> series;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        setTheme(R.style.AppTheme);
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.microphone);
+        double x,y;
+        x = -5.0;
+        GraphView graph = (GraphView) findViewById(R.id.decibellGraph);
+        series = new LineGraphSeries<DataPoint>();
+        for(int i = 0; i < 500; i++) {
+            x = x + 0.1;
+            y = Math.sin(x);
+            series.appendData(new DataPoint(x, y), true, 500);
+        }
+        graph.addSeries(series);
+    }
+
 }
